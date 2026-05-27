9d3b33955146c8ac59e5e4ddd38030bf83111f4d
shubhangNarain <snvishere@gmail.com>
2026-05-27 09:19:25 +0530

    fix: https error in fetching the api from beanstalk 2nd attempt

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   typeof window !== 'undefined' && (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
     ? 'http://localhost:8080'
-    : 'https://jauter-backend-env.eba-mgbdvgp2.ap-south-1.elasticbeanstalk.com';
+    : 'http://jauter-backend-env.eba-mgbdvgp2.ap-south-1.elasticbeanstalk.com';
